a902627e165c2b33107beca2e6908f782edd8ff6
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2024-12-22 14:53:32 +0100

    adding bibliotique chart /composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'your/project-name',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '436c66387af9015b1f69eb8ca33522f4e7ee6f7c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'markrogoyski/math-php' => array(
+            'pretty_version' => 'v2.10.0',
+            'version' => '2.10.0.0',
+            'reference' => 'e587bfe3aadddfba0f3a40f49d2ed91132f30c5a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../markrogoyski/math-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.9.3',
+            'version' => '6.9.3.0',
+            'reference' => '2f5c94fe7493efc213f643c23b1b1c249d40f47e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'your/project-name' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '436c66387af9015b1f69eb8ca33522f4e7ee6f7c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

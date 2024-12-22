bbd25230f3d748a285faa84c4ca42ab183ce6403
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2024-12-22 16:52:25 +0100

    adding approve_user

---
+++ modal/approve_user.php
@@ -0,0 +1,35 @@
+<?php
+
+require '../vendor/autoload.php'; 
+
+include('../config/database.php');
+if (isset($_GET['id'])) {
+    $userId = $_GET['id'];
+
+    $sql = "SELECT * FROM users WHERE id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $userId);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows === 1) {
+        $user = $result->fetch_assoc();
+        $userEmail = $user['email'];
+        $userName = $user['name'];
+
+        // Approve the user in the database
+        $approveSql = "UPDATE users SET is_approved = 1 WHERE id = ?";
+        $approveStmt = $conn->prepare($approveSql);
+        $approveStmt->bind_param("i", $userId);
+
+        if ($approveStmt->execute()) {
+            
+          echo "user approved";
+        } else {
+            echo "Failed to approve user.";
+        }
+    } else {
+        echo "User not found.";
+    }
+}
+?>
